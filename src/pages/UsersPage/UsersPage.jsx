5e84f2c1566c06d8e1c48be0f1f237f7bba5ee73
Andrey Alexeev <ipalexeev98@mail.ru>
2023-11-20 21:56:24 +0300

    feat: add did mount life circle [finish 54]

--- src/pages/UsersPage/UsersPage.jsx
+++ src/pages/UsersPage/UsersPage.jsx
@@ -7,18 +7,15 @@ import React from "react";
 class UsersPage extends React.Component {
   constructor(props) {
     super(props);
+  }
 
+  componentDidMount() {
     axios
       .get("https://social-network.samuraijs.com/api/1.0/users")
       .then((response) => {
         this.props.setUsers(response.data.items);
       });
   }
-  // getUsers = () => {
-  //   if (this.props.users.length === 0) {
-
-  //   }
-  // };
 
   render() {
     return (
