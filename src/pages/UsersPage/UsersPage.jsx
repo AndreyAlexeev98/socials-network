641c4fe71fe98734d1b74dbbdaf080859448a996
Andrey Alexeev <ipalexeev98@mail.ru>
2023-11-20 21:35:01 +0300

    refactor: create class component [finish 53]

--- src/pages/UsersPage/UsersPage.jsx
+++ src/pages/UsersPage/UsersPage.jsx
@@ -2,68 +2,75 @@
 import axios from "axios";
 import styles from "./UsersPage.module.scss";
 import avatarPlaceholder from "../../assets/img/avatar-placeholder.png";
+import React from "react";
 
-const UsersPage = (props) => {
-  const { users, setUsers, follow, unfollow } = props;
+class UsersPage extends React.Component {
+  constructor(props) {
+    super(props);
 
-  const getUsers = () => {
-    if (users.length === 0) {
-      axios
-        .get("https://social-network.samuraijs.com/api/1.0/users")
-        .then((response) => {
-          setUsers(response.data.items);
-        });
-    }
-  };
+    axios
+      .get("https://social-network.samuraijs.com/api/1.0/users")
+      .then((response) => {
+        this.props.setUsers(response.data.items);
+      });
+  }
+  // getUsers = () => {
+  //   if (this.props.users.length === 0) {
 
-  return (
-    <>
-      <h1>users page</h1>
-      <button onClick={getUsers}>get users</button>
-      <ul className={styles.card_list}>
-        {users.map((user, i) => (
-          <li key={user.id + i} className={styles.card}>
-            <div className={styles.preview}>
-              <img
-                className={styles.avatar}
-                src={user.photos.small ? user.photos.small : avatarPlaceholder}
-                alt="user avatar"
-              />
-              {user.followed && (
-                <button
-                  className={styles.state}
-                  onClick={() => {
-                    unfollow(user.id);
-                  }}
-                >
-                  Follow
-                </button>
-              )}
-              {!user.followed && (
-                <button
-                  className={styles.state}
-                  onClick={() => {
-                    follow(user.id);
-                  }}
-                >
-                  unFollow
-                </button>
-              )}
-            </div>
-            <div className={styles.info}>
-              <div className={styles.left}>
-                <div className={styles.name}>{user.name}</div>
-                <div className={styles.status}>{user.status}</div>
+  //   }
+  // };
+
+  render() {
+    return (
+      <>
+        <h1>users page</h1>
+        <ul className={styles.card_list}>
+          {this.props.users.map((user, i) => (
+            <li key={user.id + i} className={styles.card}>
+              <div className={styles.preview}>
+                <img
+                  className={styles.avatar}
+                  src={
+                    user.photos.small ? user.photos.small : avatarPlaceholder
+                  }
+                  alt="user avatar"
+                />
+                {user.followed && (
+                  <button
+                    className={styles.state}
+                    onClick={() => {
+                      this.props.unfollow(user.id);
+                    }}
+                  >
+                    Follow
+                  </button>
+                )}
+                {!user.followed && (
+                  <button
+                    className={styles.state}
+                    onClick={() => {
+                      this.props.follow(user.id);
+                    }}
+                  >
+                    unFollow
+                  </button>
+                )}
               </div>
-              <div className={styles.right}>
-                <div className={styles.location}>{user.id},</div>
+              <div className={styles.info}>
+                <div className={styles.left}>
+                  <div className={styles.name}>{user.name}</div>
+                  <div className={styles.status}>{user.status}</div>
+                </div>
+                <div className={styles.right}>
+                  <div className={styles.location}>{user.id},</div>
+                </div>
               </div>
-            </div>
-          </li>
-        ))}
-      </ul>
-    </>
-  );
-};
+            </li>
+          ))}
+        </ul>
+      </>
+    );
+  }
+}
 
 export default UsersPage;
